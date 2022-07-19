b6bd36d7cdae1aa2a9e4166b4dda70f6f1dd1dc7
Ng Bing Rong <ngbingrong@gmail.com>
2022-07-19 09:56:19 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/d12wkshop/D12wkshopApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.d12wkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D12wkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D12wkshopApplication.class, args);
+	}
+
+}
